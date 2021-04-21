330ca53023097949c716bff4cb49b7e7be2fde13
Frits van Lieshout <fritske05@gmail.com>
2021-04-21 17:08:00 +0200

    Fixed some issues with permissions

--- src/main/java/com/kwetter/frits/apigateway/configuration/SecurityConfig.java
+++ src/main/java/com/kwetter/frits/apigateway/configuration/SecurityConfig.java
@@ -1,10 +1,10 @@
 package com.kwetter.frits.apigateway.configuration;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsConfigurationSource;
@@ -13,20 +13,23 @@ import org.springframework.http.HttpMethod;
 
 import java.util.Arrays;
 
-@Configuration
 @EnableWebFluxSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) throws Exception {
-        http.cors().configurationSource(configCors()).and().csrf().disable()
+        return http
+                .csrf().disable()
+                .cors().configurationSource(configCors())
+                .and()
                 .authorizeExchange()
-                .pathMatchers(HttpMethod.GET, "/api/tweets/**").permitAll()
-                .pathMatchers(HttpMethod.POST, "/api/tweets/**").permitAll()
-                .anyExchange().authenticated();
-
-        return http.build();
+                    .pathMatchers(HttpMethod.GET, "/api/timeline/**").hasAuthority("timeline:read")
+                    .pathMatchers(HttpMethod.GET, "/api/tweets/**").permitAll()
+                    .pathMatchers(HttpMethod.POST, "/api/tweets/**").permitAll()
+                    .pathMatchers(HttpMethod.POST, "/api/auth/login").permitAll()
+                    .pathMatchers(HttpMethod.POST, "/api/user/register").permitAll()
+                .anyExchange().authenticated()
+                .and().build();
     }
 
     @Bean
@@ -36,11 +39,9 @@ public class SecurityConfig {
         configuration.setAllowCredentials(true);
         configuration.addAllowedOrigin("http://localhost:3000"); //https://kwetterplatform.azurewebsites.net/
         configuration.setMaxAge(3600L);
-        //configuration.addAllowedOrigin("https://kwetterplatform.azurewebsites.net");
         configuration.addAllowedHeader("*");
-        configuration.addAllowedMethod("*");
         configuration.addExposedHeader("*");
-        //configuration.addExposedHeader("Authorization");
+        configuration.addExposedHeader("Authorization");
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
